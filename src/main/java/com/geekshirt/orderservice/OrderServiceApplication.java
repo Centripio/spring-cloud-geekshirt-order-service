3cd44dd3fc58eea27ed2782abb745dfcd75417f9
Centripio <code@centripio.io>
2021-05-31 10:32:37 -0700

    [Kafka-Messaging] Adding changes to handle messaging with Kafka.

--- src/main/java/com/geekshirt/orderservice/OrderServiceApplication.java
+++ src/main/java/com/geekshirt/orderservice/OrderServiceApplication.java
@@ -6,7 +6,11 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Source;
 
+@EnableBinding({Source.class, Sink.class})
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableFeignClients
